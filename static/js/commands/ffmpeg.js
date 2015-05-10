fbe278994ed80d5473b2e3d945fba10833ae2702
Rachel Fish <itsrachelfish@gmail.com>
2015-05-10 03:49:01 -0600

    Finished ffmpeg support

--- static/js/commands/ffmpeg.js
+++ static/js/commands/ffmpeg.js
@@ -42,5 +42,75 @@ optional:
 commands.ffmpeg = function(form)
 {
     var output = ['ffmpeg'];
+
+    if(form.file.in)
+    {
+        output.push('-i ' + form.file.in);
+    }
+
+    if(parseInt(form.audio.enabled))
+    {
+        if(form.audio.bitrate)
+        {
+            output.push('-ab ' + form.audio.bitrate);
+        }
+
+        if(form.audio.codec)
+        {
+            output.push('-acodec ' + form.audio.codec);
+        }
+
+        if(form.audio.language)
+        {
+            output.push('-map 0:m:language:' + form.audio.language);
+        }
+
+        if(form.audio.channels)
+        {
+            output.push('-ac ' + form.audio.channels);
+        }
+    }
+
+    if(parseInt(form.video.enabled))
+    {
+        if(form.video.bitrate)
+        {
+            output.push('-b ' + form.video.bitrate);
+        }
+
+        if(form.video.codec)
+        {
+            output.push('-vcodec ' + form.video.codec);
+        }
+
+        if(form.video.width && form.video.height)
+        {
+            output.push('-s ' + form.video.width + "x" + form.video.height);
+        }
+    }
+
+    if(parseInt(form.subtitles.enabled))
+    {
+        if(form.subtitles.language)
+        {
+            output.push('-metadata:s:s:0 language=' + form.subtitles.language);
+        }
+    }
+
+    if(parseInt(form.misc.deinterlace))
+    {
+        output.push('-deinterlace');
+    }
+
+    if(form.misc.fps)
+    {
+        output.push('-r ' + form.misc.fps);
+    }
+    
+    if(form.file.out)
+    {
+        output.push(form.file.out);
+    }
+
     return output;
 }
